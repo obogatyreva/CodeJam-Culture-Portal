f114ca5a1222a7c5ec56049f76c1b20f7120a82b
Forkossigan <andrebakalov@mail.ru>
2019-08-01 03:01:41 +0400

    fix: fix problem

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('C:\Users\Андрей\Desktop\CULTURE PORTAL\CodeJam-Culture-Portal\node_modules\gatsby-plugin-netlify-cms\gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('C:\Users\Андрей\Desktop\CULTURE PORTAL\CodeJam-Culture-Portal\gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
